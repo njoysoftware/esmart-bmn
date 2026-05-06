2a4285f530fbb4c4955f815a6bfd02096dcc622a
njoysoftware <njoysoftware.indonesia@gmail.com>
2026-05-07 00:56:45 +0700

    modified:   app/Providers/Filament/AdminPanelProvider.php 	new file:   config/livewire.php 	modified:   vercel.json

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -34,6 +34,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->spa(false)
             ->authGuard('web')
             ->brandName('SMART BMN')
             ->login()
