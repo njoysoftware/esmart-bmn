d2ec9e93a75780ce45b27d419c35de58c0de55d9
Nizar Suryaman <114325325+njoysoftware@users.noreply.github.com>
2026-05-07 02:11:06 +0700

    Update UserResource.php

--- app/Filament/Resources/Users/UserResource.php
+++ app/Filament/Resources/Users/UserResource.php
@@ -21,7 +21,7 @@ class UserResource extends Resource
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedUsers;
 
-    protected static ?string $recordTitleAttribute = 'user';
+    protected static ?string $recordTitleAttribute = 'name';
 
     public static function form(Schema $schema): Schema
     {
