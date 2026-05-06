ce0d0689e229bed00b61791f1750a73bc7ace5f5
Nizar Suryaman <114325325+njoysoftware@users.noreply.github.com>
2026-05-07 02:37:09 +0700

    Update MaintenanceResource.php

--- app/Filament/Resources/Maintenances/MaintenanceResource.php
+++ app/Filament/Resources/Maintenances/MaintenanceResource.php
@@ -20,7 +20,7 @@ class MaintenanceResource extends Resource
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedServerStack;
 
-    protected static ?string $recordTitleAttribute = 'barang_id';
+    protected static ?string $recordTitleAttribute = 'id';
 
     public static function form(Schema $schema): Schema
     {
