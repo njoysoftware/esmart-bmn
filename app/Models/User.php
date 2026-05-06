7fab799868d9c16540a00c8a69663a58b17dd75a
njoysoftware <njoysoftware.indonesia@gmail.com>
2026-05-07 00:48:16 +0700

    modified:   app/Models/User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Notifications\Notifiable;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
