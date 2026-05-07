3a9f0e0a9569b1d7cb45f90c41856c0ac92d9743
njoysoftware <njoysoftware.indonesia@gmail.com>
2026-05-08 01:45:00 +0700

    modified:   api/index.php

--- api/index.php
+++ api/index.php
@@ -1,6 +1,12 @@
 <?php
 
 define('LARAVEL_START', microtime(true));
+// Clear cached config
+@unlink('/tmp/config.php');
+@unlink('/tmp/routes.php');
+@unlink('/tmp/events.php');
+@unlink('/tmp/packages.php');
+@unlink('/tmp/services.php');
 
 if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
     require $maintenance;
