c5f7ccc57ce088511fc4acdbe1830df670e83e21
njoysoftware <njoysoftware.indonesia@gmail.com>
2026-05-08 00:25:57 +0700

    modified:   api/index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,13 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+define('LARAVEL_START', microtime(true));
+
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->handleRequest(Illuminate\Http\Request::capture());
